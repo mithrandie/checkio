73892b625a9b843870cfdb1b1ef8c50d7c0932c8
Mithrandie <mithrandie@icloud.com>
2017-08-11 19:26:31 +0900

    Solve number-radix in python

---
+++ number-radix.py
@@ -0,0 +1,18 @@
+def checkio(str_number, radix):
+    i = -1
+
+    try:
+        i = int(str_number, radix)
+    except ValueError:
+        pass
+
+    return i
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio("AF", 16) == 175, "Hex"
+    assert checkio("101", 2) == 5, "Bin"
+    assert checkio("101", 5) == 26, "5 base"
+    assert checkio("Z", 36) == 35, "Z base"
+    assert checkio("AB", 10) == -1, "B > A = 10"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
